851aaaf2fbb6bf3f161971f10ccf5a1dc3d96987
Ramon Bonela <ramonbonela@gmail.com>
2022-06-21 14:11:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ecloud_mod4_desafio
+
+Project started in June 21, 2022.
+
+**Please, complete here information on using and testing this project.**
---
+++ infrastructure/aws/variables.tf
@@ -0,0 +1,55 @@
+variable "region_id" {
+  default = "us-east-1"
+}
+
+variable "prefix" {
+  default = "prefix"
+}
+
+variable "account" {
+  default = 774178677404
+}
+
+# Prefix configuration and project common tags
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Project      = "Desafio-Mod4-IGTI"
+    ManagedBy    = "Terraform"
+    Owner        = "Ramon Bonela"
+    BusinessUnit = "Data Engineering"
+    Environment  = terraform.workspace
+    UserEmail    = "ramonbonela@gmail.com"
+  }
+}
+
+variable "bucket_names" {
+  description = "Create S3 buckets with these names"
+  type        = list(string)
+  default = [
+    "landing-zone",
+    "processing-zone",
+    "delivery-zone"
+  ]
+}
+
+variable "database_names" {
+  description = "Create databases with these names"
+  type        = list(string)
+  default = [
+    #landing-zone
+    "dl_landing_zone",
+    "dl_rocessing_zone",
+    "dl_delivery_zone"
+  ]
+}
+
+variable "bucket_paths" {
+  description = "Paths to S3 bucket used by the crawler"
+  type        = list(string)
+  default = [
+    "s3://landing-zone-774178677404",
+    "s3://processing-zone-774178677404",
+    "s3://delivery-zone-774178677404"
+  ]
+}
