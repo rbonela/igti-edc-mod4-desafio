29aabdb537d0f8311fc6d8c66cc27840e7170776
Ramon Bonela <ramonbonela@gmail.com>
2022-06-22 13:21:39 -0300

    Updated backend.tf configuration

--- infrastructure/aws/backend.tf
+++ infrastructure/aws/backend.tf
@@ -1,7 +1,7 @@
 # Backend configuration require a AWS storage bucket.
 terraform {
   backend "s3" {
-    bucket = "s3://edc-m4-desafio-terraform-bckend"
+    bucket = "edc-m4-desafio-terraform-bckend"
     key    = "state/terraform.tfstate"
     region = "us-east-2"
   }
