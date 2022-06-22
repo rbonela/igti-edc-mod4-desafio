038c34982cc2e6e73d04f82985c4596f74a6870f
Ramon Bonela <ramonbonela@gmail.com>
2022-06-22 13:03:41 -0300

    Removed ENADE data upload from terraform deploy

--- infrastructure/aws/s3_files.tf
+++ infrastructure/aws/s3_files.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_object" "dados_enade" {
-  bucket = aws_s3_bucket.buckets[0].id
-  key    = "enade/2017/MICRODADOS_ENADE_2017"
-  acl    = "private" # or can be "public-read"
-  source = "../../dados/MICRODADOS_ENADE_2017.txt"
-  etag   = filemd5("../../dados/MICRODADOS_ENADE_2017.txt")
-}
+# resource "aws_s3_object" "dados_enade" {
+#   bucket = aws_s3_bucket.buckets[0].id
+#   key    = "enade/2017/MICRODADOS_ENADE_2017"
+#   acl    = "private" # or can be "public-read"
+#   source = "../../dados/MICRODADOS_ENADE_2017.txt"
+#   etag   = filemd5("../../dados/MICRODADOS_ENADE_2017.txt")
+# }
 
 resource "aws_s3_object" "spark-operator" {
   for_each = fileset("../../dags/enade/pyspark/", "*")
